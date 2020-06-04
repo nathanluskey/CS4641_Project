b0a98e8e6c9bff401026de701b066d425c471cc7
nilnate <34701768+nilnate@users.noreply.github.com>
2020-06-04 15:55:21 -0400

    Can search through a spotify playlist

--- SpotifyAPI/main.py
+++ SpotifyAPI/main.py
@@ -6,12 +6,17 @@ from spotipy.oauth2 import SpotifyClientCredentials
 #for os information look at https://able.bio/rhett/how-to-set-and-get-environment-variables-in-python--274rgt5
 os.environ["SPOTIPY_CLIENT_ID"] = secrets.SPOTIPY_CLIENT_ID
 os.environ["SPOTIPY_CLIENT_SECRET"] = secrets.SPOTIPY_CLIENT_SECRET
-# on MAC, use `export` on Windows use 'set'
-
 
 #Taken from https://github.com/plamere/spotipy
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+sp = spotipy.Spotify(client_credentials_manager = SpotifyClientCredentials())
 
-results = sp.search(q='weezer', limit=20)
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
+nathanPlaylist = sp.playlist("5wUsCg6wm1t5dVYMfhMgi1")
+#This is a python dict, so use .keys() repeatedly to find what we want
+
+#Printing out the first song of a playlist
+#firstSong = nathanPlaylist['tracks']['items'][0]['track']['name']
+
+#This loop prints all song names in a playlist
+for i in nathanPlaylist["tracks"]["items"]:
+    songName = i["track"]["name"]
+    print(songName)
