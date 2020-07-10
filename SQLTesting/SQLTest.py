e080ecba88aeef1ccd73a2ec65557ac9eda4564a
dreese33 <dylanjacobreese@gatech.edu>
2020-07-10 13:47:22 -0400

    Fixed some SQLTest bugs, and updated README.md

--- SQLTesting/SQLTest.py
+++ SQLTesting/SQLTest.py
@@ -1,19 +1,26 @@
 from doltpy.core import Dolt
-from doltpy.core.write import bulk_import
-from doltpy.core.read import read_table
 import os
+import sys
 
 # Check if million songs exists
-path = os.path.join(os.getcwd(),'million-songs')
-doltPath = os.path.join(path,'.dolt')
+path = os.path.join(os.getcwd(), 'million-songs')
+doltPath = os.path.join(path, '.dolt')
 repo = None
 if not os.path.isdir(doltPath):
+    if os.path.isdir(path):
+        try:
+            os.rmdir(path)
+        except Exception as exc:
+            print("Exception:", exc)
+            print("Non-empty million-songs directory exists, but no .dolt file is present. "
+                  "Delete the existing million-songs file.")
+            sys.exit()
+
     print("Initializing million-songs repository")
-    repo = Dolt.clone('Liquidata/million-songs','')
-    print("Ready for query...")
+    repo = Dolt.clone('Liquidata/million-songs', '')
 else:
     repo = Dolt(repo_dir=path)
-    print("Ready for query...")
 
+print("Ready for query...")
 print("Repository Directory:", repo.repo_dir())
 print(repo.sql(query="SHOW TABLES;"))
