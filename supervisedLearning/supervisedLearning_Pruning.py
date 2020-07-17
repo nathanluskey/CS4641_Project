2481dee94c99eb9d2e0ca0a078c58d2e4c5ca5b8
nilnate <34701768+nilnate@users.noreply.github.com>
2020-07-16 22:10:57 -0400

    Added code for pruning

--- supervisedLearning/supervisedLearning_Pruning.py
+++ supervisedLearning/supervisedLearning_Pruning.py
@@ -1,4 +1,6 @@
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 import pandas
 import os
 import pickle5 as pickle
@@ -6,69 +8,23 @@ import numpy as np
 import pydotplus
 import collections
 
-
-if __name__ == "__main__":
-    #Read in the csv
-    parentDirectory = os.path.dirname(os.getcwd())
-    songFilehandle = os.path.join(parentDirectory, "SQLTesting", "compiledGenreData_AllAttributes_5000Songs.csv")
-    csv = pandas.read_csv(songFilehandle)
-
-    #General info for tree here: https://scikit-learn.org/stable/modules/tree.html
-
-    ## Parse data for training the tree
-    #Select the column headers with 'f' features
-    # print(csv.columns) #To see all column headers
-    #Can look at example on MSD for info: 
-    # importantFeatures = list(['artist_name', 'duration', 'energy', 'key', 'loudness', 'mode', 'end_of_fade_in', 'mode', 'song_hotttnesss', 'start_of_fade_out', 'tempo', 'time_signature', 'year'])
-    # TODO: For now just look at numerical data
-    importantFeatures = list(['year', 'tempo', 'duration', 'loudness', 'mode', 'key'])
-    allFeatures = np.empty(0)
-    for featureTitle in importantFeatures:
-        feature = csv[featureTitle]
-        feature = np.array(feature)
-        feature = feature.reshape((-1, 1))
-        # print("Feature: {}, allFeatures.size = {}, feature.shape = {}, allFeatures.shape = {}".format(featureTitle, allFeatures.size, feature.shape, allFeatures.shape))
-        if allFeatures.size == 0:
-            #initialize it
-            allFeatures = feature
-        else:
-            #stack onto it
-            allFeatures = np.concatenate((allFeatures, feature), axis=1)
-    #allFeatures is now an Nxf array with 'f' features and 'N' samples
-    #Select the column of 'genre' for the labels
-    genreLabels = np.array(csv['genre'])
-    uniqueGenres = np.unique(genreLabels)
-    #Initialize the tree
-    #Documentation: https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html#sklearn.tree.DecisionTreeClassifier
-    #TODO: Impliment a pruned decision tree
-    #Example: https://scikit-learn.org/stable/auto_examples/tree/plot_cost_complexity_pruning.html#sphx-glr-download-auto-examples-tree-plot-cost-complexity-pruning-py
-    clf = None
-    
-    #Save the tree by pickling the model for persistence https://scikit-learn.org/stable/modules/model_persistence.html
-    with open("clf_pruned.pickle", "wb") as f:
-        pickle.dump(clf, f)
-
-    # Visualize data
-    # Can use max_depth to better visualize all data
+def saveGraph(clf, filename, max_depth=None):
+# Can use max_depth to better visualize all data
     dot_data = export_graphviz(clf,
                                 feature_names=importantFeatures,
                                 class_names=uniqueGenres,
+                                max_depth=max_depth,
                                 out_file=None,
                                 filled=True,
                                 rounded=True)
-    #Using graphviz
-    # graph = graphviz.Source(dot_data)
-    # graph.render("trainingOnYear")
     
     
     #Using pydotplus
     graph = pydotplus.graph_from_dot_data(dot_data)
     nodes = graph.get_node_list()
-    edges = graph.get_edge_list()
 
     #Create list of unique colors for each genre
     colors = ('red', 'green', 'blue', 'orange', 'yellow', 'fuchsia', 'green', 'lightseagreen', 'silver', 'coral', 'lightpink', 'mediumpurple','lightsteelblue','mediumvioletred','peachpuff','thistle', 'white')
-    edges = collections.defaultdict(list)
 
     for node in nodes:
         if node.get_name() not in ('node', 'edge'):
@@ -81,9 +37,73 @@ if __name__ == "__main__":
                 #If not a leaf node, color white
                 node.set_fillcolor(colors[-1])
 
-    graph.write_png('trainingOnMultipleFeatures_LargeTree.png')
+    graph.write_png(filename)
     print("i, Colors, and Genre")
 
     for i in range(len(uniqueGenres)):
         print("\ti = {}, Color = {}, Genre = {}".format(i, colors[i], uniqueGenres[i]))
     
+
+
+if __name__ == "__main__":
+    #Read in the csv, assuming starting on the most local directory
+    parentDirectory = os.path.dirname(os.getcwd())
+    songFilehandle = os.path.join(parentDirectory, "SQLTesting", "compiledGenreData_AllAttributes_5000Songs.csv")
+    csv = pandas.read_csv(songFilehandle)
+    #General info for tree here: https://scikit-learn.org/stable/modules/tree.html
+
+    ## Parse data for training the tree
+    importantFeatures = ['year', 'tempo', 'duration', 'loudness', 'mode', 'key']
+    allFeatures = np.array(csv.loc[:, importantFeatures].values)
+    genreLabels = np.array(csv.loc[:, 'genre'])
+    uniqueGenres = np.unique(genreLabels)
+    #Initialize the tree
+    #TODO: Impliment a pruned decision tree
+    #Example: https://scikit-learn.org/stable/auto_examples/tree/plot_cost_complexity_pruning.html#sphx-glr-download-auto-examples-tree-plot-cost-complexity-pruning-py
+    
+    ## Split data into train and test
+    X_train, X_test, y_train, y_test = train_test_split(allFeatures, genreLabels, random_state=0)
+
+    clf = DecisionTreeClassifier(criterion="entropy")
+
+    #First look at alphas
+    path = clf.cost_complexity_pruning_path(X_train, y_train)
+    ccp_alphas, impurities = path.ccp_alphas, path.impurities
+    #Train decision trees with the different ccp_alphas
+    clfs = []
+    for ccp_alpha in ccp_alphas:
+        clf = DecisionTreeClassifier(random_state=0, ccp_alpha=ccp_alpha)
+        clf.fit(X_train, y_train)
+        clfs.append(clf)
+    print("Number of nodes in the last tree is: {} with ccp_alpha: {}".format(
+        clfs[-1].tree_.node_count, ccp_alphas[-1]))
+    #Remove trivial case of 1 node
+    clfs = clfs[:-1]
+    ccp_alphas = ccp_alphas[:-1]
+    #Compare accuracy on training and testing set
+    train_scores = [clf.score(X_train, y_train) for clf in clfs]
+    test_scores = [clf.score(X_test, y_test) for clf in clfs]
+
+    fig, ax = plt.subplots()
+    ax.set_xlabel("alpha")
+    ax.set_ylabel("accuracy")
+    ax.set_title("Accuracy vs alpha for training and testing sets")
+    ax.plot(ccp_alphas, train_scores, marker='o', label="train",
+            drawstyle="steps-post")
+    ax.plot(ccp_alphas, test_scores, marker='o', label="test",
+            drawstyle="steps-post")
+    ax.legend()
+    plt.show()
+    plt.savefig("AlphaVSAccuracy.png")
+    
+    #TODO: Save and show the optimal tree
+    if False:
+        #From the graph take the max ccp_alpha training set
+        print("Decision Tree Depth: {}".format(clf.get_depth()))
+        #Save the tree by pickling the model for persistence
+        with open("clf_pruned.pickle", "wb") as f:
+            pickle.dump(clf, f)
+        # Save a visualization of data
+        filename = 'trainingOnMultipleFeatures_LargeTree_Pruned.png'
+        saveGraph(clf, filename, max_depth=2)
+        print("Tree saved to: \n\t{}".format(filename))
